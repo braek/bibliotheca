92ff53bc681ffce79e4bbce59addacd0b9de0f55
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-03-23 08:33:35 +0100

    WIP

--- src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/addbook/AddBookController.java
+++ src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/addbook/AddBookController.java
@@ -1,22 +1,17 @@
 package be.koder.library.rest.addbook;
 
 import be.koder.library.api.AddBook;
-import be.koder.library.api.presenter.AddBookPresenter;
-import be.koder.library.rest.ErrorResponse;
 import be.koder.library.vocabulary.book.Author;
-import be.koder.library.vocabulary.book.BookId;
 import be.koder.library.vocabulary.book.Isbn;
 import be.koder.library.vocabulary.book.Title;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/v1")
-public class AddBookController implements AddBookPresenter {
+public class AddBookController {
 
     private final AddBook addBook;
-    private ResponseEntity<Object> response;
 
     public AddBookController(AddBook addBook) {
         this.addBook = addBook;
@@ -30,15 +25,5 @@ public class AddBookController implements AddBookPresenter {
         final var presenter = new AddBookRestPresenter();
         addBook.addBook(isbn, title, author, presenter);
         return presenter.getResponse();
-    }
-
-    @Override
-    public void added(BookId bookId) {
-        response = ResponseEntity.status(HttpStatus.CREATED).body(new AddBookResponse(bookId.getValue()));
-    }
-
-    @Override
-    public void isbnAlreadyExists() {
-        response = ResponseEntity.badRequest().body(new ErrorResponse("Het ISBN bestaat al."));
     }
 }
--- src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/addbook/AddBookRestPresenter.java
+++ src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/addbook/AddBookRestPresenter.java
@@ -1,24 +1,28 @@
 package be.koder.library.rest.addbook;
 
 import be.koder.library.api.presenter.AddBookPresenter;
+import be.koder.library.rest.ErrorResponse;
 import be.koder.library.rest.RestPresenter;
 import be.koder.library.vocabulary.book.BookId;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public final class AddBookRestPresenter implements RestPresenter, AddBookPresenter {
 
+    private ResponseEntity<Object> response;
+
     @Override
     public void added(BookId bookId) {
-
+        response = ResponseEntity.status(HttpStatus.CREATED).body(new AddBookResponse(bookId.getValue()));
     }
 
     @Override
     public void isbnAlreadyExists() {
-
+        response = ResponseEntity.badRequest().body(new ErrorResponse("Het ISBN bestaat al."));
     }
 
     @Override
     public ResponseEntity<Object> getResponse() {
-        return null;
+        return response;
     }
 }
