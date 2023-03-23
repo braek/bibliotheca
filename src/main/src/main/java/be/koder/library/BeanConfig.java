a1327d670582e0c8b6334943fd1121d1e9d74657
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-03-23 09:53:59 +0100

    WIP

--- src/main/src/main/java/be/koder/library/BeanConfig.java
+++ src/main/src/main/java/be/koder/library/BeanConfig.java
@@ -1,12 +1,20 @@
 package be.koder.library;
 
 import be.koder.library.api.AddBook;
+import be.koder.library.api.ModifyBook;
+import be.koder.library.api.RemoveBook;
+import be.koder.library.api.SetHardcover;
 import be.koder.library.domain.book.BookRepository;
+import be.koder.library.domain.book.HardcoverStore;
 import be.koder.library.domain.book.IsbnService;
 import be.koder.library.domain.event.EventPublisher;
 import be.koder.library.inmemory.InMemoryBookRepository;
 import be.koder.library.inmemory.InMemoryEventPublisher;
+import be.koder.library.inmemory.InMemoryHardcoverStore;
 import be.koder.library.usecase.addbook.AddBookUseCase;
+import be.koder.library.usecase.modifybook.ModifyBookUseCase;
+import be.koder.library.usecase.removebook.RemoveBookUseCase;
+import be.koder.library.usecase.sethardcover.SetHardcoverUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,5 +34,25 @@ public class BeanConfig {
     @Bean
     AddBook addBook(BookRepository bookRepository, IsbnService isbnService, EventPublisher eventPublisher) {
         return new AddBookUseCase(bookRepository, isbnService, eventPublisher);
+    }
+
+    @Bean
+    ModifyBook modifyBook(BookRepository bookRepository, EventPublisher eventPublisher) {
+        return new ModifyBookUseCase(bookRepository, eventPublisher);
+    }
+
+    @Bean
+    HardcoverStore hardcoverStore() {
+        return new InMemoryHardcoverStore();
+    }
+
+    @Bean
+    RemoveBook removeBook(BookRepository bookRepository, EventPublisher eventPublisher) {
+        return new RemoveBookUseCase(bookRepository, eventPublisher);
+    }
+
+    @Bean
+    SetHardcover setHardcover(BookRepository bookRepository, HardcoverStore hardcoverStore, EventPublisher eventPublisher) {
+        return new SetHardcoverUseCase(bookRepository, hardcoverStore, eventPublisher);
     }
 }
