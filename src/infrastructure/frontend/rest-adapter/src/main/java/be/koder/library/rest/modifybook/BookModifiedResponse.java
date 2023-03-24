785efc34f8185e766f3f85be1bb8b7865b06862a
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-03-24 22:07:07 +0100

    WIP

--- src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/modifybook/BookModifiedResponse.java
+++ src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/modifybook/BookModifiedResponse.java
@@ -1,4 +1,8 @@
 package be.koder.library.rest.modifybook;
 
-public record BookModifiedResponse() {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.UUID;
+
+public record BookModifiedResponse(@Schema(requiredMode = Schema.RequiredMode.REQUIRED) UUID bookId) {
 }
--- src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/modifybook/ModifyBookRestPresenter.java
+++ src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/modifybook/ModifyBookRestPresenter.java
@@ -1,8 +1,10 @@
 package be.koder.library.rest.modifybook;
 
 import be.koder.library.api.presenter.ModifyBookPresenter;
+import be.koder.library.rest.ErrorResponse;
 import be.koder.library.rest.RestPresenter;
 import be.koder.library.vocabulary.book.BookId;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public final class ModifyBookRestPresenter implements ModifyBookPresenter, RestPresenter {
@@ -11,12 +13,12 @@ public final class ModifyBookRestPresenter implements ModifyBookPresenter, RestP
 
     @Override
     public void modified(BookId bookId) {
-
+        response = ResponseEntity.status(HttpStatus.OK).body(new BookModifiedResponse(bookId.getValue()));
     }
 
     @Override
     public void bookNotFound() {
-
+        response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse("Book does not exist."));
     }
 
     @Override
