4d3cc36765acb426e0a03e547b96e13109baa7c2
Kristof Verbraeken <kristof.verbraeken@vlaanderen.be>
2023-03-24 22:33:34 +0100

    WIP

--- src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/sethardcover/SetHardcoverEndpoint.java
+++ src/infrastructure/frontend/rest-adapter/src/main/java/be/koder/library/rest/sethardcover/SetHardcoverEndpoint.java
@@ -58,7 +58,7 @@ public interface SetHardcoverEndpoint {
                                             implementation = ErrorResponse.class
                                     )
                             )
-                    ),
+                    )
             }
     )
     @PostMapping(
